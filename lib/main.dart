79a4065e88b7e9ce6cbd1a827f1d74c55ba692dd
pvgonzales <pvgonzales@up.edu.ph>
2024-05-12 04:41:07 +0800

    chore: added routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_project/model/org_model.dart';
+import 'package:flutter_project/screens/home/home.dart';
 import 'package:flutter_project/screens/intro-screen/intro.dart';
+import 'package:flutter_project/screens/organization-details/orgdetails.dart';
+import 'package:flutter_project/screens/sign-in/signin.dart';
+import 'package:flutter_project/screens/sign-up/completedetails.dart';
 import 'package:flutter_project/screens/sign-up/signup.dart';
 
 void main() {
@@ -24,8 +29,17 @@ class MyApp extends StatelessWidget {
           return MaterialPageRoute(builder: (context) => const IntroScreen());
         }else if(setting.name == '/sign-up'){
           return MaterialPageRoute(builder: (context) => const SignUpScreen());
-        }else{
+        }else if (setting.name == '/sign-in'){
+          return MaterialPageRoute(builder: (context) => const SignInScreen());
+        }else if (setting.name == '/home'){
+          return MaterialPageRoute(builder: (context) => const HomeScreen());
+        }else if (setting.name == '/org-details'){
+          var orgname = setting.arguments as Organizations;
+          return MaterialPageRoute(builder: (context) => OrgDonation(org: orgname));
+        }else if (setting.name == '/forgot-password'){
           return MaterialPageRoute(builder: (context) => const Placeholder());
+        }else if (setting.name == '/complete-profile'){
+          return MaterialPageRoute(builder: (context) => const CompleteDetailsScreen());
         }
       },
     );
