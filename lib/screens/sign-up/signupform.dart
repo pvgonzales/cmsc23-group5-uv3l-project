ac7b662e59f5130d6a9724cadd6855c27b894d2e
pvgonzales <pvgonzales@up.edu.ph>
2024-05-11 22:41:22 +0800

    chore: added the sign-up page

---
+++ lib/screens/sign-up/signupform.dart
@@ -0,0 +1,104 @@
+import 'package:flutter/material.dart';
+
+class SignUpForm extends StatefulWidget {
+  const SignUpForm({super.key});
+
+  @override
+  _SignUpFormState createState() => _SignUpFormState();
+}
+
+class _SignUpFormState extends State<SignUpForm> {
+  final _formKey = GlobalKey<FormState>();
+  String? email;
+  String? password;
+  String? confirmPassword;
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: _formKey,
+      child: Column(
+        children: [
+          TextFormField(
+            keyboardType: TextInputType.emailAddress,
+            onSaved: (newValue) => email = newValue,
+            onChanged: (value) {
+              if (value.isNotEmpty) {
+              } else if (value.contains("@")) { // SUBJECT TO CHANGE
+              }
+              return;
+            },
+            validator: (value) {
+              if (value!.isEmpty) {
+                return "";
+              } else if (!value.contains("@")) { // SUBJECT TO CHANGE
+                return "Please enter a valid email";
+              }
+              return null;
+            },
+            decoration: const InputDecoration(
+              labelText: "Email",
+              hintText: "Enter your email",
+              floatingLabelBehavior: FloatingLabelBehavior.always,
+              suffixIcon: Icon(Icons.email) ,
+            ),
+          ),
+          const SizedBox(height: 20),
+          TextFormField(
+            obscureText: true,
+            onSaved: (newValue) => password = newValue,
+            onChanged: (value) {
+              password = value;
+            },
+            validator: (value) {
+              if (value!.isEmpty) {
+                return "Please enter your password";
+              } else if (value.length < 8) {
+                return "Password must be at least 8 characters";
+              }
+              return null;
+            },
+            decoration: const InputDecoration(
+              labelText: "Password",
+              hintText: "Enter your password",
+              floatingLabelBehavior: FloatingLabelBehavior.always,
+              suffixIcon: Icon(Icons.password),
+            ),
+          ),
+          const SizedBox(height: 20),
+          TextFormField(
+            obscureText: true,
+            onSaved: (newValue) => confirmPassword = newValue,
+            onChanged: (value) {
+              confirmPassword = value;
+            },
+            validator: (value) {
+              if (value!.isEmpty) {
+                return "Please enter your password";
+              } else if ((password != value)) {
+                return "Passwords do not match";
+              }
+              return null;
+            },
+            decoration: const InputDecoration(
+              labelText: "Confirm Password",
+              hintText: "Re-enter your password",
+              floatingLabelBehavior: FloatingLabelBehavior.always,
+              suffixIcon: Icon(Icons.lock_rounded),
+            ),
+          ),
+          const SizedBox(height: 20),
+          ElevatedButton(
+            onPressed: () {
+              if (_formKey.currentState!.validate()) {
+                _formKey.currentState!.save();
+                Navigator.pushNamed(context, '/sign-in');
+              }
+            },
+            child: const Text("Continue"),
+          ),
+        ],
+      ),
+    );
+  }
+}
