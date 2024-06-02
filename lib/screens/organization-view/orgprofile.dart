60f80d649b1f85287beb944fdc23ffb935da0a7c
pvgonzales <pvgonzales@up.edu.ph>
2024-06-02 15:27:01 +0800

    chore: modified the fetching of organization user data and modified the state to update status

--- lib/screens/organization-view/orgprofile.dart
+++ lib/screens/organization-view/orgprofile.dart
@@ -1,12 +1,13 @@
+import 'package:auto_size_text/auto_size_text.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:flutter_project/provider/auth_provider.dart';
 import 'package:flutter_project/provider/orgdrive_provider.dart';
 import 'package:provider/provider.dart';
-import 'package:flutter_project/model/user_model.dart';
 
 class OrgProfile extends StatefulWidget {
-  final bool? initialStatus;
-  const OrgProfile({super.key, this.initialStatus});
+  const OrgProfile({super.key});
 
   @override
   State<OrgProfile> createState() => _OrgProfileState();
@@ -45,213 +46,234 @@ class _OrgProfileState extends State<OrgProfile> {
     }
   }
 
-  @override
-  Widget build(BuildContext context) {
-    final userModel = Provider.of<UserModel>(context);
-    final userAuthProvider = Provider.of<UserAuthProvider>(context);
+@override
+Widget build(BuildContext context) {
+  final UserAuthProvider userProvider =
+      Provider.of<UserAuthProvider>(context, listen: false);
+  final OrganizationProvider organizationProvider =
+      Provider.of<OrganizationProvider>(context, listen: false);
 
-    final user = userAuthProvider.user;
-    if (user != null) {
-      userAuthProvider.fetchUserData(user.uid, context);
-    }
+  String? username = userProvider.currentUsername;
 
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: const Color(0xFF212738),
-        title: const Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            SizedBox(height: 30),
-            Row(
+  return FutureBuilder(
+    future: organizationProvider.fetchOrganizationByUsername(username!),
+    builder: (context, snapshot) {
+      if (snapshot.connectionState == ConnectionState.waiting) {
+        return Center(child: CircularProgressIndicator());
+      } else if (snapshot.hasError) {
+        return Center(child: Text('Error: ${snapshot.error}'));
+      } else {
+        final user = organizationProvider.organizations;
+
+        return Scaffold(
+          appBar: AppBar(
+            backgroundColor: const Color(0xFF212738),
+            title: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                SizedBox(
-                  width: 50,
-                ),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  mainAxisSize: MainAxisSize.min,
+                const SizedBox(height: 30),
+                Row(
                   children: [
-                    Text(
-                      "Organization Profile",
-                      style: TextStyle(
-                        fontFamily: "MyFont1",
-                        color: Color.fromARGB(255, 248, 249, 252),
-                        fontWeight: FontWeight.w900,
-                        fontSize: 23,
-                      ),
+                    const SizedBox(
+                      width: 50,
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Text(
+                          user[0].name,
+                          style: const TextStyle(
+                            fontFamily: "MyFont1",
+                            color: Color.fromARGB(255, 248, 249, 252),
+                            fontWeight: FontWeight.w900,
+                            fontSize: 23,
+                          ),
+                        ),
+                      ],
                     ),
                   ],
                 ),
+                SizedBox(height: 20),
               ],
             ),
-            SizedBox(height: 20),
-          ],
-        ),
-        toolbarHeight: 150,
-        automaticallyImplyLeading: false,
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(20.0),
-        ),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.only(top: 40, bottom: 20, left: 30, right: 30),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            const CircleAvatar(
-              backgroundColor: Colors.transparent,
-              radius: 60,
-              backgroundImage: AssetImage('assets/images/orglogo3.png'),
+            toolbarHeight: 150,
+            automaticallyImplyLeading: false,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(20.0),
             ),
-            const SizedBox(height: 20),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                const Text(
-                  'Full Name:',
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 15,
-                    fontFamily: "MyFont1",
+          ),
+          body: Padding(
+            padding: const EdgeInsets.only(top: 40, bottom: 20, left: 30, right: 30),
+            child: user.isNotEmpty
+                ? Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      const CircleAvatar(
+                        backgroundColor: Colors.transparent,
+                        radius: 60,
+                        backgroundImage: AssetImage('assets/images/orglogo3.png'),
+                      ),
+                      const SizedBox(height: 20),
+                      Flexible(
+                        child: AutoSizeText(
+                          //"Angat Buhay Foundation, incorporated as Angat Pinas, Inc., is a non-profit, non-governmental organization based in the Philippines. It was founded and officially launched on July 1, 2022, a day after its founder Leni Robredo's term as Vice President of the Philippines expired.",
+                          user[0].description,
+                          style: const TextStyle(
+                            fontFamily: "MyFont1",
+                            fontWeight: FontWeight.w900,
+                            fontSize: 13,
+                          ),
+                          textAlign: TextAlign.center,
+                          maxLines: 5,
+                          minFontSize: 13,
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                      ),
+                      const SizedBox(height: 20),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          const Text(
+                            'Username:',
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 15,
+                              fontFamily: "MyFont1",
+                            ),
+                          ),
+                          Text(
+                            user[0].username,
+                            style: const TextStyle(
+                              fontSize: 13,
+                              fontFamily: "MyFont1",
+                            ),
+                          ),
+                        ],
+                      ),
+                      const SizedBox(height: 10),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          const Text(
+                            'Orginzation Type:',
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 15,
+                              fontFamily: "MyFont1",
+                            ),
+                          ),
+                          Text(
+                            user[0].type,
+                            style: const TextStyle(
+                              fontSize: 13,
+                              fontFamily: "MyFont1",
+                            ),
+                          ),
+                        ],
+                      ),
+                      const SizedBox(height: 10),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          const Text(
+                            'Phone Number:',
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 15,
+                              fontFamily: "MyFont1",
+                            ),
+                          ),
+                          Text(
+                            user[0].contact,
+                            style: const TextStyle(
+                              fontSize: 13,
+                              fontFamily: "MyFont1",
+                            ),
+                          ),
+                        ],
+                      ),
+                      const SizedBox(height: 10),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          const Text(
+                            'Address:',
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 15,
+                              fontFamily: "MyFont1",
+                            ),
+                          ),
+                          Text(
+                            user[0].address,
+                            style: const TextStyle(
+                              fontSize: 13,
+                              fontFamily: "MyFont1",
+                            ),
+                          ),
+                        ],
+                      ),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      Center(
+                        child: Column(
+                          children: [
+                            const Text('Status for accepting donations'),
+                            Switch(
+                              thumbIcon: thumbIcon,
+                              value: user[0].approved == false ? false : user[0].status,
+                              onChanged: (bool value) {
+                                if (user[0].approved == true){
+                                  setState(() {
+                                    status = value;
+                                  });
+                                  Provider.of<OrganizationProvider>(context, listen: false)
+                                    .updateOrganizationStatus(user[0].uid, status);
+                                }
+                              },
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  )
+                : const Center(
+                    child: Text(
+                      'No organization data available.',
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontFamily: "MyFont1",
+                      ),
+                    ),
                   ),
                 ),
-                Text(
-                  userModel.fullname ?? 'N/A',
-                  style: const TextStyle(
-                    fontSize: 13,
-                    fontFamily: "MyFont1",
-                  ),
-                ),
-              ],
-            ),
-            const SizedBox(height: 10),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                const Text(
-                  'Username:',
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 15,
-                    fontFamily: "MyFont1",
-                  ),
-                ),
-                Text(
-                  userModel.username ?? 'N/A',
-                  style: const TextStyle(
-                    fontSize: 13,
-                    fontFamily: "MyFont1",
-                  ),
-                ),
-              ],
-            ),
-            const SizedBox(height: 10),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                const Text(
-                  'Phone Number:',
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 15,
-                    fontFamily: "MyFont1",
-                  ),
-                ),
-                Text(
-                  userModel.phoneNumber ?? 'N/A',
-                  style: const TextStyle(
-                    fontSize: 13,
-                    fontFamily: "MyFont1",
-                  ),
-                ),
-              ],
-            ),
-            const SizedBox(height: 10),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                const Text(
-                  'Address:',
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 15,
-                    fontFamily: "MyFont1",
-                  ),
-                ),
-                Text(
-                  userModel.address ?? 'N/A',
-                  style: const TextStyle(
-                    fontSize: 13,
-                    fontFamily: "MyFont1",
-                  ),
-                ),
-              ],
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            Center(
-              child: Column(
-                children: [
-                  const Text('Status for accepting donations'),
-                  Switch(
-                    thumbIcon: thumbIcon,
-                    value: status,
-                    onChanged: (bool value) {
-                      setState(() {
-                        status = value;
-                      });
-                      Provider.of<OrganizationProvider>(context, listen: false)
-                      .updateOrganizationStatus(3, status); // 3 is a hardcoded value, it must be the id of the 'organization' user
-                    },
-                  ),
-                ],
-              ),
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            Center(
-              child: TextButton(
-                onPressed: () {
-                  context.read<UserAuthProvider>().authService.signOut();
-                  Navigator.pushNamed(context, "/");
-                },
-                child: const Text(
-                  'Logout',
-                  style: TextStyle(
-                    color: Colors.red,
-                    fontSize: 15,
-                    fontFamily: "MyFont1",
-                  ),
-                ),
-              ),
-            ),
-          ],
-        ),
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        backgroundColor: const Color(0xFF212738),
+                bottomNavigationBar: BottomNavigationBar(
+                  backgroundColor: const Color(0xFF212738),
 
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: 'Home',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.domain_verification_rounded),
-            label: 'Drives',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.person),
-            label: 'Profile',
-          ),
-        ],
-        currentIndex: 2, // Set the current index to indicate the profile screen
-        onTap: (index) => _onItemTapped(context, index),
-        selectedItemColor: const Color.fromARGB(255, 243, 164, 160),
-        unselectedItemColor: Colors.white,
-      ),
-    );
+                  items: const <BottomNavigationBarItem>[
+                    BottomNavigationBarItem(
+                      icon: Icon(Icons.home),
+                      label: 'Home',
+                    ),
+                    BottomNavigationBarItem(
+                      icon: Icon(Icons.domain_verification_rounded),
+                      label: 'Drives',
+                    ),
+                    BottomNavigationBarItem(
+                      icon: Icon(Icons.person),
+                      label: 'Profile',
+                    ),
+                  ],
+                  currentIndex: 2, // Set the current index to indicate the profile screen
+                  onTap: (index) => _onItemTapped(context, index),
+                  selectedItemColor: const Color.fromARGB(255, 243, 164, 160),
+                  unselectedItemColor: Colors.white,
+                ),
+            );
+          }
+        },
+      );
+    }
   }
-}
