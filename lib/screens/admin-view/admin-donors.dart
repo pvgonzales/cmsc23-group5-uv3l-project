ce1157c9152eaa04e1f45f81ce24eb94efc567aa
KLeoncio <kbleoncio@up.edu.ph>
2024-06-03 18:15:18 +0800

    ui: updated donors' screen ui

--- lib/screens/admin-view/admin-donors.dart
+++ lib/screens/admin-view/admin-donors.dart
@@ -22,7 +22,7 @@ class DonorsScreen extends StatelessWidget {
               ),
             ),
           ),
-          Expanded(
+          Expanded( 
             child: Padding(
               padding: const EdgeInsets.only(left: 20, right: 20),
               child: Consumer<UserAuthProvider>(
